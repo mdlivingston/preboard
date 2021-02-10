df3d1774c4c52ac1524050b10cc7a771a225b1d9
Maximus <developermaximus@gmail.com>
2021-02-09 22:42:21 -0600

    - added question creation

--- src/Exams/Exam.js
+++ src/Exams/Exam.js
@@ -32,36 +32,57 @@ export default function Exam()
 
     const { examId } = useParams()
 
-    useEffect(() =>
+    function getQuestions(index) 
     {
         db.questions
             .where("examId", "==", examId)
-            //.orderBy("createdAt")
+            .orderBy("createdAt")
             .get()
             .then(querySnapshot =>
             {
                 const data = querySnapshot.docs.map(doc => db.formatDoc(doc));
 
+                // No questions
                 if (data.length === 0)
+                {
+                    addQuestion()
                     return
+                }
 
                 setQuestions(data)
-                setEditorState(data[0].html)
-                setQuestionId(data[0].id)
-                setQuestionIndex(0)
+                console.log(1)
+
+                // First load with questions
+                if (!questionId)
+                {
+                    setEditorState(data[0].html)
+                    setQuestionId(data[0].id)
+                    setQuestionIndex(0)
+                }
+                else // After first load
+                {
+                    setEditorState(data[index].html)
+                    setQuestionId(data[index].id)
+                }
             });
+    }
+
+    useEffect(() =>
+    {
+        getQuestions(0)
     }, [])
+
 
     return (
         <div className="exam-page">
             <div className="sidebar">
                 <div className="sidebar-section"><b>Questions</b></div>
-                <div className="sidebar-section">1</div>
-                <div className="sidebar-section">2</div>
-                <div className="sidebar-section">3</div>
-                <div className="sidebar-section">4</div>
-                <div className="sidebar-section">5</div>
-
+                {!questions && <div className="sidebar-section">1</div>}
+                {questions && questions.map((q, i) =>
+                (
+                    <div onClick={() => goToQuestion(i)} className="sidebar-section">{i + 1}</div>
+                ))}
+                <div onClick={addQuestion} className="sidebar-section"> + </div>
             </div>
             <div className="question-editor">
                 <h3> Question {questionIndex + 1}</h3>
@@ -77,15 +98,15 @@ export default function Exam()
                         {
                             const data = editor.getData();
                             setEditorState(data)
-                            console.log({ event, editor, data });
+                            //console.log({ event, editor, data });
                         }}
                         onBlur={(event, editor) =>
                         {
-                            console.log('Blur.', editor);
+                            //console.log('Blur.', editor);
                         }}
                         onFocus={(event, editor) =>
                         {
-                            console.log('Focus.', editor);
+                            //console.log('Focus.', editor);
                         }}
                     />
                 </div>
@@ -98,41 +119,56 @@ export default function Exam()
     function saveEditorData()
     {
         if (questionId)
-        {
-            db.questions.doc(questionId).set({
-                html: editorState,
-                examId
-                //userId: User.uid
-            })
-                .then(function ()
-                {
-                    console.log("Document successfully written!");
-                })
-                .catch(function (error)
-                {
-                    console.error("Error writing document: ", error);
-                });
-        }
+            editQuestion()
         else
-        {
-            db.questions.add({
-                html: editorState,
-                examId,
-                createdAt: db.getCurrentTimeStamp(),
-                //userId: User.uid
+            addQuestion()
+    }
+
+    function goToQuestion(index)
+    {
+        setQuestionId(questions[index].id)
+        setQuestionIndex(index)
+        setEditorState(questions[index].html)
+    }
+
+    function addQuestion()
+    {
+        setQuestionIndex(questions ? questions.length : 0)
+        setEditorState("")
+
+        db.questions.add({
+            html: "",
+            examId,
+            createdAt: db.getCurrentTimeStamp(),
+            //userId: User.uid
+        })
+            .then(function (questionData)
+            {
+                getQuestions(questions.length)
+                console.log("Document successfully written!");
             })
-                .then(function (questionData)
-                {
-                    setQuestionId(questionData.id)
-                    console.log("Document successfully written!");
-                })
-                .catch(function (error)
-                {
-                    console.error("Error writing document: ", error);
-                });
-        }
+            .catch(function (error)
+            {
+                console.error("Error writing document: ", error);
+            });
+    }
 
-
+    function editQuestion()
+    {
+        db.questions.doc(questionId).set({
+            html: editorState,
+            examId,
+            //userId: User.uid
+        }, { merge: true })
+            .then(function ()
+            {
+                getQuestions(questionIndex)
+                console.log("Document successfully written!");
+            })
+            .catch(function (error)
+            {
+                console.error("Error writing document: ", error);
+            });
     }
 
 }
