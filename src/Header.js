f501d262326bec19a227f707b4634f8dd9b09906
Maximus <developermaximus@gmail.com>
2021-02-10 17:36:27 -0600

    - added menu for profile button

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Dropdown } from 'react-bootstrap'
 import logo from './logo.svg';
+
 export default function Header()
 {
     return (
@@ -10,9 +11,19 @@ export default function Header()
             Preboard Exams
             <img className={'App-logo'} src={logo} alt="React"></img>
             <span style={{ flex: 1 }}></span>
-            <Button variant="success">
+            <Dropdown className="profile-btn">
+                <Dropdown.Toggle variant="success" >
+                    <FontAwesomeIcon icon={faUser} />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item>Profile</Dropdown.Item>
+                    <Dropdown.Item>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            {/* <Button variant="success">
                 <FontAwesomeIcon icon={faUser} />
-            </Button>
+            </Button> */}
 
         </div>
     )
