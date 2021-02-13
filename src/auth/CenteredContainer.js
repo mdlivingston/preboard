e0b0f37c117071f933f3bee5f92fff8dbca2cd3d
Maximus <developermaximus@gmail.com>
2021-02-12 23:34:18 -0600

    - centered container

--- src/auth/CenteredContainer.js
+++ src/auth/CenteredContainer.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap'
 export default function CenteredContainer({ children })
 {
     return (
-        <Container className="d-flex align-items-center justify-content-center" style={{ height: '50%', flexDirection: 'column' }}>
+        <Container className="d-flex align-items-center justify-content-center" style={{ height: '100%', flexDirection: 'column' }}>
             <div className="w-100" style={{ maxWidth: "400px" }}>
                 {children}
             </div>
